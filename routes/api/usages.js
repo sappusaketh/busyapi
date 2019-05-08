059c20be145f8d5728cdf816bb5dec8a5d0288d7
sappusaketh <sakethkumarkappala@gmail.com>
2019-05-08 18:03:10 -0400

    updated Usages

--- routes/api/usages.js
+++ routes/api/usages.js
@@ -1,12 +1,21 @@
-module.exports = function(app){
-    app.post('/api/usages', function(req, res){
+const patient = require('../../model/patient');
 
-        // Store the supplied usage data
-        app.usages.push(req.body);
-
-        var usageId = app.usages.length;
-        console.log('Stored usage count: ' + usageId);
-
-        res.status(201).json({'id':usageId});
-    });
+async function addToDB(data) {
+  const pushdata = [...data];
+  await patient.insertMany(pushdata);
 }
+let count = 0;
+module.exports = function(app) {
+  app.post('/api/usages', function(req, res) {
+    // Store the supplied usage data
+    app.usages.push(req.body);
+
+    var usageId = app.usages.length;
+    console.log('Stored usage count: ' + usageId);
+    if (app.usages.length >= 1000) {
+      addToDB(app.usages);
+      app.usages = [];
+    }
+    res.status(201).json({ id: usageId });
+  });
+};
